a1cf4b7f91f40befc5da989cc4743e435976a7eb
Vladimir Umek <vladimir.umek@arm.com>
2022-12-13 11:35:08 +0100

    Use Open-CMSIS-Pack/gen-pack as pack generator

---
+++ gen_pack.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Version: 1.0
+# Date: 2022-12-12
+# This bash script generates a CMSIS-FreeRTOS Software Pack
+#
+
+set -o pipefail
+
+# Set version of gen pack library
+REQUIRED_GEN_PACK_LIB="0.6.0"
+
+# Set default command line arguments
+DEFAULT_ARGS=(-c "v")
+
+# Pack warehouse directory - destination
+PACK_OUTPUT=./output
+
+# Temporary pack build directory
+PACK_BUILD=./build
+
+# Specify directory names to be added to pack base directory
+PACK_DIRS="
+  CMSIS
+  Config
+  Documentation
+  License
+  Source
+"
+
+# Specify file names to be added to pack base directory
+PACK_BASE_FILES="
+"
+
+# Specify file names to be deleted from pack build directory
+PACK_DELETE_FILES=""
+
+# Specify patches to be applied
+PACK_PATCH_FILES=""
+
+# Specify addition argument to packchk
+PACKCHK_ARGS=()
+
+# Specify additional dependencies for packchk
+PACKCHK_DEPS="
+  ARM.CMSIS.pdsc
+"
+
+# Optional: restrict fallback modes for changelog generation
+# Default: full
+# Values:
+# - full      Tag annotations, release descriptions, or commit messages (in order)
+# - release   Tag annotations, or release descriptions (in order)
+# - tag       Tag annotations only
+PACK_CHANGELOG_MODE="full"
+
+# custom preprocessing steps
+function preprocess() {
+  ./DoxyGen/gen_doc.sh
+}
+
+# custom post-processing steps
+# function postprocess() {
+# }
+
+############ DO NOT EDIT BELOW ###########
+
+function install_lib() {
+  local URL="https://github.com/Open-CMSIS-Pack/gen-pack/archive/refs/tags/v$1.tar.gz"
+  echo "Downloading gen-pack lib to '$2'"
+  mkdir -p "$2"
+  curl -L "${URL}" -s | tar -xzf - --strip-components 1 -C "$2" || exit 1
+}
+
+function load_lib() {
+  if [[ -d ${GEN_PACK_LIB} ]]; then
+    . "${GEN_PACK_LIB}/gen-pack"
+    return 0
+  fi
+  local GLOBAL_LIB="/usr/local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  local USER_LIB="${HOME}/.local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  if [[ ! -d "${GLOBAL_LIB}" && ! -d "${USER_LIB}" ]]; then
+    echo "Required gen_pack lib not found!" >&2
+    install_lib "${REQUIRED_GEN_PACK_LIB}" "${USER_LIB}"
+  fi
+
+  if [[ -d "${GLOBAL_LIB}" ]]; then
+    . "${GLOBAL_LIB}/gen-pack"
+  elif [[ -d "${USER_LIB}" ]]; then
+    . "${USER_LIB}/gen-pack"
+  else
+    echo "Required gen-pack lib is not installed!" >&2
+    exit 1
+  fi
+}
+
+load_lib
+gen_pack "${DEFAULT_ARGS[@]}" "$@"
+
+exit 0
